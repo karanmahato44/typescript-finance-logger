1a5a2fe3a91a46430105bf7488efaf74b7af0870
Karan Mahato <44karanmahato@gmail.com>
2023-09-21 21:17:14 +0545

    generics

--- public/app.js
+++ public/app.js
@@ -20,3 +20,34 @@ form.addEventListener('submit', (e) => {
     }
     record.render(doc, paymentType.value, 'end');
 });
+/* // GENERICS
+
+// const addUser = <T extends { name: string }>(obj: T) => {
+const addUser = <T extends object>(obj: T) => {
+  let uid = Math.floor(Math.random() * 10);
+  return { ...obj, uid }
+};
+
+const user = addUser({ name: 'karan', age: 22 });
+const user2 = addUser({ foo: 'bar' });
+console.log(user.age); // not possible without generics÷
+
+// GENERICS WITH INTERFACES
+
+interface Resource<T> {
+  name: string;
+  age: number;
+  data: T
+}
+
+const someResource: Resource<string> = {
+  name: 'karan',
+  age: 22,
+  data: 'lorem ipsum foo bar'
+}
+
+const someOtherRes: Resource<number[]> = {
+  name: 'kyto',
+  age: 44,
+  data: [1, 2, 3, 4, 5]
+} */
--- src/app.ts
+++ src/app.ts
@@ -25,3 +25,37 @@ form.addEventListener('submit', (e: Event) => {
   }
   record.render(doc, paymentType.value, 'end');
 })
+
+
+/* // GENERICS
+
+// const addUser = <T extends { name: string }>(obj: T) => {
+const addUser = <T extends object>(obj: T) => {
+  let uid = Math.floor(Math.random() * 10);
+  return { ...obj, uid }
+};
+
+const user = addUser({ name: 'karan', age: 22 });
+const user2 = addUser({ foo: 'bar' });
+console.log(user.age); // not possible without generics÷
+
+// GENERICS WITH INTERFACES
+
+interface Resource<T> {
+  name: string;
+  age: number;
+  data: T
+}
+
+const someResource: Resource<string> = {
+  name: 'karan',
+  age: 22,
+  data: 'lorem ipsum foo bar'
+}
+
+const someOtherRes: Resource<number[]> = {
+  name: 'kyto',
+  age: 44,
+  data: [1, 2, 3, 4, 5]
+} */
+
